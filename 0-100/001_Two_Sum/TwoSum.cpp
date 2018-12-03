05a03b2ce2bf727dc4c4bda2a500989c50cf7cf6
yang <2212371178@qq.com>
2018-12-03 10:54:19 +0800

    Update TwoSum.cpp

--- 0-100/001_Two_Sum/TwoSum.cpp
+++ 0-100/001_Two_Sum/TwoSum.cpp
@@ -1,1 +1,23 @@
 
+//使用hash表，时间复杂度O(n),运行时间4ms
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j,len=nums.size();
+        vector <int> ans;
+        map <int,int> hash;
+        for(i=0;i<len;i++)
+            hash[nums[i]]=i;
+        for(i=0;i<len;i++)
+        {
+           auto iter=hash.find(target-nums[i]);
+           if(iter!=hash.end()&&iter->second!=i)
+           {
+               ans.push_back(i);
+               ans.push_back(iter->second);
+               return ans;
+           }
+        }
+        return ans;
+    }
+};
