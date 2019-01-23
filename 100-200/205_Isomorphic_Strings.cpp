9be23660525d35abf671050ce309ae3cade946cf
yangjie <2212371178@qq.com>
2019-01-23 20:18:52 +0800

    Update 205_Isomorphic_Strings.cpp

--- 100-200/205_Isomorphic_Strings.cpp
+++ 100-200/205_Isomorphic_Strings.cpp
@@ -1,3 +1,4 @@
+/*给两个字符串问它们是不是相似的字符串，比如“egg”和"add"就是相似的字符串，这题的解法是利用哈希表*/
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
@@ -6,11 +7,11 @@ public:
         if(s.size()!=t.size()) return false;
         if(s==t) return true;
         for(i=0;i<len;i++)
-        if(hash.count(s[i])==0) hash[s[i]]=t[i];
-        else if(hash[s[i]]!=t[i]) return false;
+         if(hash.count(s[i])==0) hash[s[i]]=t[i];
+         else if(hash[s[i]]!=t[i]) return false;
         hash.clear();
         for(i=0;i<len;i++)
-        if(hash.count(t[i])==0) hash[t[i]]=s[i];
+         if(hash.count(t[i])==0) hash[t[i]]=s[i];
          else if(hash[t[i]]!=s[i]) return false;
         return true;
     }
