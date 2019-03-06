b6b04886747907b9475bba32772953f5ac8e0559
yangjie <2212371178@qq.com>
2019-03-06 21:06:28 +0800

    Update 040_Combination_Sum_II.cpp

--- 001-100/040_Combination_Sum_II.cpp
+++ 001-100/040_Combination_Sum_II.cpp
@@ -1,5 +1,5 @@
 /*在array中找出所有加起来等于target的组合，组合不可以重复。可以先给array排序，再用回溯的方法。每次都代入一个数字，直到等于target或者大于target，
-倒退回去一个数字，继续递归。和039类似，不同的是本题要求数组中的元素不可以重复用，要注意回溯参数变化，以及去重。*/
+倒退回去一个数字，继续递归。和039类似，不同的是本题要求数组中的元素不可以重复用，要注意回溯参数变化，i变成i+1。以及遇到重复的数字跳过*/
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
@@ -20,7 +20,7 @@ public:
         }
         for(i=index;i<candidates.size();i++)
         {
-            if(i!=0&&candidates[i]==candidates[i-1]&&i>index) continue;//避免出现重复组合
+            if(i!=0&&candidates[i]==candidates[i-1]&&i>index) continue;//跳过避免出现重复组合
             temp.push_back(candidates[i]);
             backstracking(ans,temp,candidates, target-candidates[i],i+1);//i变成i+1
             temp.pop_back();
