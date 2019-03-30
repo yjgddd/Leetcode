81178b7b33184e2ab18c78aff80938bc9313f5b5
yangjie <2212371178@qq.com>
2019-03-30 21:46:54 +0800

    Create 206*_Reverse_Linked_List.cpp

---
+++ 201-300/206*_Reverse_Linked_List.cpp
@@ -0,0 +1,25 @@
+/*单链表的逆值，待总结思路*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* L=NULL,*M=NULL,*R=head;
+        if(head==NULL) return NULL;
+        while(R->next!=NULL)
+        {
+            L=M;
+            M=R;
+            R=R->next;
+            M->next=L;    
+        }
+        R->next=M;
+        return R;
+    }
+};
