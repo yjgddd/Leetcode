463e2b0b3c3d29cc4105ddaf0cbded7963acc58a
yangjie <2212371178@qq.com>
2019-04-02 20:41:13 +0800

    Create 104_Maximum_Depth_of_Binary_Tree.cpp

---
+++ 101-200/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,17 @@
+/*求树的最大深度，简单递归*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        else return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
