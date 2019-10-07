1aedcc0110b05a9ce68d36de9ee7b3f0d5ba5c71
yangjie <2212371178@qq.com>
2019-10-07 16:29:05 +0800

    Create 121_Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ 101-200/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,14 @@
+//简单题，什么时候买进卖出获得利润最多
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()<=1) return 0;
+        int buy=prices[0],ans=0;
+        for(int i=0;i<prices.size()-1;i++)
+          for(int j=i+1;j<prices.size();j++){
+              if(prices[i]<buy) buy=prices[i];
+              if(prices[j]-buy>ans) ans=prices[j]-buy;
+          }
+        return ans;
+    }
+};
